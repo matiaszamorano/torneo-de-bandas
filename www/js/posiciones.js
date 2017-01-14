598b031cb71abbcbf3a7ec03de35534fd26f653b
Uva <Uva@Uva-PC>
2017-01-14 19:48:26 -0300

    Listo el top de bandas

--- www/js/posiciones.js
+++ www/js/posiciones.js
@@ -1,8 +1,22 @@
 var posiciones = (function () {
 
-    var urlSitio = "http://www.comunidadfusa.com";
-    
+    var url = "http://www.comunidadfusa.com/servicios/juegos/torneo-de-bandas/posiciones";
+
     function init() {
+        $.ajax({
+            type: "GET",
+            dataType: "json",
+            url: url,
+            cache: false,
+            success: mostrarPosiciones
+        });
+    }
+
+    function mostrarPosiciones(data) {
+        var templateBandas = $.templates("#templateBandas");
+        var html = templateBandas.render(data);
+        $("#bandas").html(html);
+        console.log(data);
     }
 
     return {
