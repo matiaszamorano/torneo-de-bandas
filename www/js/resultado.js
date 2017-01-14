133c64b10c5521133652a194f1921b61f4971355
Uva <Uva@Uva-PC>
2017-01-14 17:54:34 -0300

    Guardo en local storage el resultado del partido

--- www/js/resultado.js
+++ www/js/resultado.js
@@ -1,12 +1,23 @@
 var resultado = (function () {
+
+    var urlSitio = "http://www.comunidadfusa.com";
     function init() {
         var resultado = window.localStorage.getItem("resultado");
         var audios = JSON.parse(resultado);
+        console.log(audios);
         if (audios.length == 1) {
-            console.log("gano uno");
+            mostrarGanador(audios);
         } else {
-            console.log("empate");
+            mostrarEmpate(audios);
         }
+    }
+
+    function mostrarGanador(audios) {
+        var ganador = audios[0];
+        $("#resultadoPlayer01 a img").attr("src", urlSitio + ganador.avatar);
+        $("#nombreGanador .nombreBandaGanadora a").text(ganador.nombreBanda);
+        $("#nombreGanador .nombreBandaGanadora a").attr("href", urlSitio + "/bandas/" + ganador.url_fusa);
+        $("#nombreGanador .nombreTema").text(ganador.nombreTema);
     }
 
     return {
